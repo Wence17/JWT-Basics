014d8e4c2b1b549eac2c4d247e47f08d0e721f7e
Wenceslaus Obimka <obimkasomto@gmail.com>
2024-10-12 11:44:03 -0500

    first commit

--- controllers/main.js
+++ controllers/main.js
@@ -1,6 +1,13 @@
+const CustomAPIErrors = require('../errors/custom-error')
+
 const login = async (req, res) => {
     const {username,password} = req.body;
     console.log(username,password);
+
+    if(!username || !password){
+        throw new CustomAPIErrors("Please provide email and password", 400);
+        
+    }
     
     res.send("fake signin/register/signup");
   };
